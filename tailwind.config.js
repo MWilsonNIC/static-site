073359c36144d9f45e3aaccc1a945facf723dab3
MWilsonNIC <mwilson@nic.bc.ca>
2026-03-11 15:00:32 -0700

    Replace all files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                tracks: "url('./images/tracks-2.png')",
+            },
+        },
+    },
+    plugins: [],
+}
+
